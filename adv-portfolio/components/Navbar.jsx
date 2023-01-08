3e3fd80043bf696461f5a0cf2e77102e8faa039b
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-01-08 21:18:35 +0530

    Project Setup(with tailwind)

---
+++ adv-portfolio/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-full h-20 bg-blue-500'>
+        <div className='flex w-full h-full items-center justify-center bg-blue-50' >
+            <h1>Navbar</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
