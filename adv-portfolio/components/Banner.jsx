dd4af4ca555a557bd7e16d9edd4bc61ab25481d2
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-01-15 23:55:46 +0530

    Fixed Scale

--- adv-portfolio/components/Banner.jsx
+++ adv-portfolio/components/Banner.jsx
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
-
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 function Banner() {
     return (
         <div className='w-full h-screen mt-[5rem]'>
             <div className='flex flex-col items-center w-full  max-w-[1240px]  mx-auto p-4 justify-center '>
-                <div className=' bg-blend-difference   relative  rounded-lg  w-full h-[30vh] md:h-[50vh] min-w-[40vh] bg-[#20202a] opacity-100 bg-right-top bg-cover bg-no-repeat' style={{ backgroundImage: "url('/assets/mum1.jpg')" }}>
+                <div className=' bg-blend-difference   relative  rounded-lg  w-full h-[40vh] md:h-[50vh] min-w-[40vh] bg-[#20202a] opacity-100 bg-right-top bg-cover bg-no-repeat ease-in duration-500' style={{ backgroundImage: "url('/assets/mum1.jpg')" }}>
                     <div className='absolute top-0 left-0 w-full h-full bg-gray-900/50'></div>
                     <div className=' z-[1] absolute bottom-3 text-white p-2 flex items-center justify-center w-full h-full'>
                         <h1 className='text-5xl lg:text-8xl'>Discover my Amazing Work-Space</h1>
@@ -15,8 +18,8 @@ function Banner() {
 
                     </div>
                 </div>
-                <div className='z-[2] grid grid-cols-2 lg:grid-cols-4  gap-4 py-[20px] w-full '>
-                    <div className='flex items-center border-[1px] border-gray-500 hover:bg-[#252a34] justify-around  bg-[#15181E] shadow-md hover:shadow-lg hover:shadow-gray-900 hover:translate-y-[-0.2rem] rounded-lg p-[35px] ease-in duration-200'>
+                <div className='z-[2] grid grid-cols-2 lg:grid-cols-4  gap-1 py-[10px] w-full '>
+                    <div className='flex items-center border-[1px] border-gray-500 hover:bg-[#252a34] justify-around  bg-[#15181E] shadow-md hover:shadow-lg hover:shadow-gray-900  rounded-lg p-[35px] ease-in duration-200'>
                         <div>
                             <p className='text-[#FFC107] font-bold text-xl'>2+</p>
                         </div>
@@ -24,7 +27,7 @@ function Banner() {
                             <p className='font-bold text-xl'>Experience</p>
                         </div>
                     </div>
-                    <div className='flex items-center border-[1px] border-gray-500 hover:bg-[#252a34] justify-around bg-[#15181E] shadow-md hover:shadow-lg hover:shadow-gray-900 hover:translate-y-[-0.2rem] rounded-lg p-[35px] ease-in duration-200'>
+                    <div className='flex items-center border-[1px] border-gray-500 hover:bg-[#252a34] bg-[#15181E] justify-around shadow-md hover:shadow-lg hover:shadow-gray-900  rounded-lg p-[35px] ease-in duration-200'>
                         <div>
                             <p className='text-[#FFC107] font-bold text-xl'>2+</p>
                         </div>
@@ -32,7 +35,7 @@ function Banner() {
                             <p className='font-bold text-xl'>Experience</p>
                         </div>
                     </div>
-                    <div className='flex items-center border-[1px] border-gray-500 hover:bg-[#252a34] justify-around bg-[#15181E] shadow-md hover:shadow-lg hover:shadow-gray-900 hover:translate-y-[-0.2rem] rounded-lg p-[35px] ease-in duration-200'>
+                    <div className='flex items-center border-[1px] border-gray-500 hover:bg-[#252a34] justify-around bg-[#15181E] shadow-md hover:shadow-lg hover:shadow-gray-900  rounded-lg p-[35px] ease-in duration-200'>
                         <div>
                             <p className='text-[#FFC107] font-bold text-xl'>2+</p>
                         </div>
@@ -40,7 +43,7 @@ function Banner() {
                             <p className='font-bold text-xl'>Experience</p>
                         </div>
                     </div>
-                    <div className='flex items-center border-[1px] border-gray-500 hover:bg-[#252a34] justify-around bg-[#15181E] shadow-md hover:shadow-lg hover:shadow-gray-900 hover:translate-y-[-0.2rem] rounded-lg p-[35px] ease-in duration-200'>
+                    <div className='flex items-center border-[1px] border-gray-500 hover:bg-[#252a34] justify-around bg-[#15181E] shadow-md hover:shadow-lg hover:shadow-gray-900  rounded-lg p-[35px] ease-in duration-200'>
                         <div>
                             <p className='text-[#FFC107] font-bold text-xl'>2+</p>
                         </div>
@@ -49,6 +52,36 @@ function Banner() {
                         </div>
                     </div>
             
+                </div> 
+                <div className='flex items-center justify-between w-full  md:w-[50%] m-auto py-4 ease-in duration-500'>
+                    <a
+                        href='/'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        <div className='mx-[8px] rounded-full shadow-lg shadow-gray-900 p-4 md:p-9 cursor-pointer hover:text-white  border-[1px] border-gray-500  bg-[#15181E] overflow-hidden ease-in duration-300'>
+                            <FaLinkedinIn size={25} className=' hover:scale-110 ease-in duration-300' />
+                        </div>
+                    </a>
+                    <a
+                        href='/'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        <div className='mx-[8px] rounded-full shadow-lg shadow-gray-900 p-4 md:p-9 cursor-pointer hover:text-white  border-[1px] border-gray-500  bg-[#15181E] ease-in duration-300'>
+                            <FaGithub size={25} className=' hover:scale-110 ease-in duration-300'  />
+                        </div>
+                    </a>
+                    <Link href='/#contact'>
+                        <div className='mx-[8px] rounded-full shadow-lg shadow-gray-900 p-4 md:p-9 cursor-pointer hover:text-white  border-[1px] border-gray-500  bg-[#15181E] ease-in duration-300'>
+                            <AiOutlineMail size={25} className=' hover:scale-110 ease-in duration-300'  />
+                        </div>
+                    </Link>
+                    <Link href='/'>
+                        <div className='mx-[8px] rounded-full shadow-lg shadow-gray-900 p-4 md:p-9 cursor-pointer hover:text-white  border-[1px] border-gray-500  bg-[#15181E] ease-in duration-300'>
+                            <BsFillPersonLinesFill size={25} className=' hover:scale-110 ease-in duration-300'  />
+                        </div>
+                    </Link>
                 </div>
 
 
