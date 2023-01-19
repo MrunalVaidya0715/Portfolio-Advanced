c218ea1d6c86aae992d3b5fd15e363f52539ab68
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-01-19 21:50:19 +0530

    Added Project layout

--- adv-portfolio/components/Projects.jsx
+++ adv-portfolio/components/Projects.jsx
@@ -3,20 +3,30 @@ import Video from './Video'
 
 function Projects() {
     return (
-        <div className=' relative'>
+        <div className=' relative flex w-full h-screen border-2 border-white items-center justify-center'>
             <div className='m-0 p-0  absolute left-0 top-0 flex w-full h-full items-center justify-center overflow-hidden'>
-        <img src='assets/svg/product-lines-bg.svg' alt="/"  className='bg-center bg-cover ' />
+                <img src='assets/svg/product-lines-bg.svg' alt="/" className='bg-center bg-cover ' />
 
-      </div>
-            <div className='z-[100] relative top-0 left-0 flex w-screen h-screen max-w-[1240px] items-center justify-center'>
+            </div>
+            <div className='z-[1] relative top-0 left-0 flex w-screen h-screen max-w-[1240px] items-center justify-center'>
+                <div className=' absolute left-0 top-0'>
+                    <p className=' uppercase text-xl tracking-widest text-[#5651e5]'>
+                        &lt;Projects&gt;
+                    </p>
+                </div>
                 <div className='border-2 border-white w-full h-full flex  items-center justify-center'>
                     <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
                         <div className='flex border-[1px] rounded-xl bg-gray-900/90 backdrop-blur-xl border-gray-500 w-[45vh] h-[60vh]' >
                             <h1>Mrunal</h1>
                         </div>
-                        
+
                     </div>
 
+                </div>
+                <div className=' absolute left-0 bottom-0'>
+                    <p className=' uppercase text-xl tracking-widest text-[#5651e5]'>
+                        &lt;Projects&gt;
+                    </p>
                 </div>
             </div>
         </div>
--- adv-portfolio/components/Skills.jsx
+++ adv-portfolio/components/Skills.jsx
@@ -4,81 +4,81 @@ import Video from './Video'
 
 function Skills() {
   return (
-    <div id='skills' className=' relative w-full h-screen flex items-center justify-center border-2 border-white'>
-      <div className='m-0 p-0  absolute left-0 top-0 flex w-full h-full items-center justify-center overflow-hidden'>
+    <div id='skills' className=' relative w-full h-screen flex items-center justify-center'>
+      <div className= 'm-0 p-0  absolute left-0 top-0 flex w-full h-full items-center justify-center overflow-hidden'>
         <video src='assets/videos/backVideo.mp4' alt="/" autoPlay loop muted className='bg-center bg-cover  transform-gpu ' />
 
       </div>
-    <div className='  z-[90] max-w-[1240px] w-full m-auto p-4 relative top-0 left-0 overflow-hidden  '>
-      
-      <div className=' relative flex justify-center w-full h-full flex-col'>
-        <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
-          &lt;Skills&gt;
-        </p>
+      <div className=' z-[90] max-w-[1240px] w-full m-auto p-4 relative top-0 left-0 overflow-hidden  '>
 
-        <div className='grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4  gap-8 py-[20px] '>
-          <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[1rem] ease-in duration-200'>
-            <img src='assets/skills/html.png' width="65" height="50" alt="Not found" />
-            <div>
-              <p className='font-bold text-xl ml-4  text-white'>HTML</p>
+        <div className=' flex justify-center w-full h-full flex-col'>
+          <p className=' uppercase text-xl tracking-widest text-[#5651e5]'>
+            &lt;Skills&gt;
+          </p>
+
+          <div className='grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4  gap-8 py-[20px] '>
+            <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[1rem] ease-in duration-200'>
+              <img src='assets/skills/html.png' width="65" height="50" alt="Not found" />
+              <div>
+                <p className='font-bold text-xl ml-4  text-white'>HTML</p>
+              </div>
             </div>
-          </div>
-          <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
-            <img src='assets/skills/css.png' width="65" height="50" alt="Not found" />
-            <div>
-              <p className='font-bold text-xl ml-4  text-white '>CSS</p>
+            <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
+              <img src='assets/skills/css.png' width="65" height="50" alt="Not found" />
+              <div>
+                <p className='font-bold text-xl ml-4  text-white '>CSS</p>
+              </div>
             </div>
-          </div>
-          <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
-            <img src='assets/skills/javascript.png' width="65" height="50" alt="Not found" />
-            <div>
-              <p className='font-bold text-xl ml-4  text-white'>JAVASCRIPT</p>
+            <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
+              <img src='assets/skills/javascript.png' width="65" height="50" alt="Not found" />
+              <div>
+                <p className='font-bold text-xl ml-4  text-white'>JAVASCRIPT</p>
+              </div>
             </div>
-          </div>
-          <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
-            <img src='assets/skills/react.png' width="65" height="50" alt="Not found" />
-            <div>
-              <p className='font-bold text-xl ml-4  text-white'>REACT JS</p>
+            <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
+              <img src='assets/skills/react.png' width="65" height="50" alt="Not found" />
+              <div>
+                <p className='font-bold text-xl ml-4  text-white'>REACT JS</p>
+              </div>
             </div>
-          </div>
-          <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
-            <img src='assets/skills/nextjs.png' width="65" height="50" alt="Not found" />
-            <div>
-              <p className='font-bold text-xl ml-4  text-white'>NEXT JS</p>
+            <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
+              <img src='assets/skills/nextjs.png' width="65" height="50" alt="Not found" />
+              <div>
+                <p className='font-bold text-xl ml-4  text-white'>NEXT JS</p>
+              </div>
             </div>
-          </div>
-          <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
-            <img src='assets/skills/tailwind.png' width="65" height="50" alt="Not found" />
-            <div>
-              <p className='font-bold text-xl ml-4  text-white'>TAILWIND CSS</p>
+            <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
+              <img src='assets/skills/tailwind.png' width="65" height="50" alt="Not found" />
+              <div>
+                <p className='font-bold text-xl ml-4  text-white'>TAILWIND CSS</p>
+              </div>
             </div>
-          </div>
-          <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
-            <img src='assets/skills/node.png' width="65" height="50" alt="Not found" />
-            <div>
-              <p className='font-bold text-xl ml-4  text-white'>NODE JS</p>
+            <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
+              <img src='assets/skills/node.png' width="65" height="50" alt="Not found" />
+              <div>
+                <p className='font-bold text-xl ml-4  text-white'>NODE JS</p>
+              </div>
             </div>
-          </div>
 
-          <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
-            <img src='assets/skills/mongo.png' width="65" height="50" alt="Not found" />
-            <div>
-              <p className='font-bold text-xl ml-4  text-white'>MONGO DB</p>
+            <div className='flex items-center justify-around bg-[#15181E]/80 backdrop-blur-md  hover:bg-[#252a34] border-[1px] border-gray-500 shadow-md hover:shadow-2xl rounded-lg p-[2rem] ease-in duration-200'>
+              <img src='assets/skills/mongo.png' width="65" height="50" alt="Not found" />
+              <div>
+                <p className='font-bold text-xl ml-4  text-white'>MONGO DB</p>
+              </div>
             </div>
+
+
+
+
+
+
+
           </div>
-
-
-
-
-
-
-
+          <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
+            &lt;/Skills&gt;
+          </p>
         </div>
-        <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
-          &lt;/Skills&gt;
-        </p>
       </div>
-    </div>
     </div>
 
 
